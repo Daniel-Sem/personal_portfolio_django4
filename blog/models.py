16e34449c60a74ed86934d245eee9a42d849df7b
Daniel Semerdzhiev <d.c.semerdjiev@gmail.com>
2022-03-15 09:58:12 +0200

    Initial record - functional but ugly

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from turtle import title
+from django.db import models
+
+class Blog(models.Model):
+
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    date = models.DateField()
+
+    def __str__(self):
+        return self.title
