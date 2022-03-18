83c7819adfc5523d987dab6fd7e318b621277894
Daniel-Sem <98486266+Daniel-Sem@users.noreply.github.com>
2022-03-18 09:22:33 +0200

    Update settings.py

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -130,7 +130,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-#per_port
-#per_port1
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
